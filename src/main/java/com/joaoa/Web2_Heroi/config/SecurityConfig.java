005889dc8152e719f69eac3cac201eb60ca93e15
João Augusto Marciano Silva <68918598+JoaoAugusto2020@users.noreply.github.com>
2025-06-05 09:00:34 -0300

    commit7.1
    Pequenas correções de autorização no botão de Listar

--- src/main/java/com/joaoa/Web2_Heroi/config/SecurityConfig.java
+++ src/main/java/com/joaoa/Web2_Heroi/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
 
         http.authorizeHttpRequests(requests -> requests
             .requestMatchers("/home", "/register", "/saveUser").permitAll()
+            //.requestMatchers("/heroi/list").hasAuthority("Manager")
             .requestMatchers("/heroi/*").hasAuthority("Admin")
             .anyRequest().authenticated())
                 .formLogin(login ->
